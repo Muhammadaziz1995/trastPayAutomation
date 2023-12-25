02d2e90c37234f41bd054191dadccfa216795164
Muhammadaziz1995 <67625177+Muhammadaziz1995@users.noreply.github.com>
2023-12-25 05:39:18 +0500

    Update login_flow.py

--- flows/android/login_flow.py
+++ flows/android/login_flow.py
@@ -19,15 +19,13 @@ class LoginFlow(AndroidBaseFlow):
         self.welcome_screen.enter_password(password)
         self.welcome_screen.click_on_davom_etish_button()
         assert self.welcome_screen.is_otp_tasdiqlash_screen_open()
+        self.p2p_screen.enter_otp('998112')
         # assert self.welcome_screen.is_entered_phone_number_exist(phone_number)
         time.sleep(6)
         assert self.welcome_screen.is_pin_code_screen_open()
         self.welcome_screen.create_pin_code()
         assert self.welcome_screen.is_confirm_pin_code_screen_open()
         self.welcome_screen.create_pin_code()
-        if self.welcome_screen.is_dear_user_message_appear():
-            assert True
-        else:
-            self.screens.get_a_screenshot_as_PNG("Loan.png")
+        assert self.welcome_screen.is_dear_user_message_appear()
         time.sleep(2)
         return True
