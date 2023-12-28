09a490982e22c6a23773faa4298193a79b603fd0
Muhammadaziz1995 <mxoliqberdiyev@gmail.com>
2023-12-28 14:34:02 +0500

    simple test

--- tests/test_suites/android/test_suit_loan.py
+++ tests/test_suites/android/test_suit_loan.py
@@ -2,8 +2,7 @@ import allure
 from allure_commons.types import AttachmentType
 
 
-@allure.title("Kreditni muddatidan oldin so'ndirish")
 class TestSuitLoan:
-    def test_case_loan(self, create_loan_flow_user_a):
+    def test_suit_loan(self, create_loan_flow_user_a):
         user_a = create_loan_flow_user_a
         assert user_a.check_advanced_repayment('Bank', '1000', '5802', '0', '47008827', '1 000')
